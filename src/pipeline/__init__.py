d648ea33f23b8220af8aef3651927a4479e65512
Samuel Hinton <samuelreay@gmail.com>
2025-08-28 19:40:06 +1000

    Adding process run

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -2,14 +2,17 @@ from pipeline.config.deployment import registry
 from pipeline.config.global_settings import settings
 from pipeline.flows.extract_snifs_run_logs import parse_snifs_runs_logs
 from pipeline.flows.preprocess_exposure import preprocess_exposure
+from pipeline.flows.process_run import process_run
 
 __all__ = [
     "settings",
     "preprocess_exposure",
     "parse_snifs_runs_logs",
+    "process_run",
     "registry",
 ]
 all_flows = [
     preprocess_exposure,
     parse_snifs_runs_logs,
+    process_run,
 ]
