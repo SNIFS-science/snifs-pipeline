f506a212786dc094d154e26a6df91660f20088e2
Samuel Hinton <samuelreay@gmail.com>
2025-08-29 15:49:16 +1000

    More fixes

--- src/pipeline/common/prefect_utils.py
+++ src/pipeline/common/prefect_utils.py
@@ -90,7 +90,7 @@ async def run_deployment(
                             logger = get_logger()
                             logger.info(f"Returning result artifact for flow run id {flow_run_id}")
                             logger.info(f"Artifact data is: {artifacts[0].data}")
-                            return json.loads(str(artifacts[0].data).replace("```", ""))
+                            return json.loads(str(artifacts[0].data).replace("```", "").replace("\n", ""))
                 except PrefectHTTPStatusError:
                     pass
     return None
